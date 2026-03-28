583323eb2f8ca6d06855c768a5285727c170b0d3
Manoj Gowda B G <manojrohinigowda1@gmail.com>
2026-03-28 09:05:54 +0530

    Remove build artifacts and large OSM file from tracking

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/GoogleMap
-# Build directory: D:/GoogleMap/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(GMapTests "D:/GoogleMap/build/gmap_tests.exe")
-set_tests_properties(GMapTests PROPERTIES  _BACKTRACE_TRIPLES "D:/GoogleMap/CMakeLists.txt;29;add_test;D:/GoogleMap/CMakeLists.txt;0;")
